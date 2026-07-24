37e1d8f919624918c9be38a2138be0c9230bb94c
DRE4M <fjfjone@gmail.com>
2026-07-25 02:34:24 +0900

    fix: update copyright in default.vue and verify Faq.vue GitHub URLs

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,12 +3,7 @@
     <slot />
   </div>
   <div class="px-2 pb-10 text-xs text-gray-700 dark:text-slate-400 text-center">
-    Pokémon © 2002-{{ new Date().getFullYear() }} Pokémon. © 1995-{{
-      new Date().getFullYear()
-    }}
-    Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names
-    are trademarks of Nintendo.<br />
-    No copyright or trademark infringement is intended in using Pokémon content
-    on Pokémantle.
+    League of Legends © {{ new Date().getFullYear() }} Riot Games, Inc. Champdle isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.<br />
+    No copyright or trademark infringement is intended in using League of Legends content on Champdle.
   </div>
 </template>
